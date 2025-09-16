b7a15e5a96e4f9d57dea4495a80fd727682d6536
Aryang92 <aramuring92@gmail.com>
2025-09-16 21:47:12 +0800

    Chat And Comment App

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
-import CounterApp from './CounterApp';
-import ColorChangerApp from './ColorChangerApp';
-//import ChatAndCommentApp from './ChatAndCommentApp';
+//import CounterApp from './CounterApp';
+//import ColorChangerApp from './ColorChangerApp';
+import ChatAndCommentApp from './ChatAndCommentApp';
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <ColorChangerApp />
-      <CounterApp/>
-      {/*<ChatAndCommentApp/>*/}
+     {/* <ColorChangerApp /> */}
+      {/*<CounterApp/> */}
+      <ChatAndCommentApp/>
     </View>
   );
 };
