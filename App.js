5f2386a94ac10c77c021896b8901dffeafb3a073
Aryang92 <aramuring92@gmail.com>
2025-10-29 17:11:07 +0800

    Updated App.js with new feature

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { View } from 'react-native';
 //import CounterApp from './CounterApp';
 //import ColorChangerApp from './ColorChangerApp';
-import ChatAndCommentApp from './ChatAndCommentApp';
-
+//import ChatAndCommentApp from './ChatAndCommentApp';
+import ChatHead from "./ChatHead";
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
      {/* <ColorChangerApp /> */}
       {/*<CounterApp/> */}
-      <ChatAndCommentApp/>
+     {/* <ChatAndCommentApp/>*/}
+       <ChatHead/>
     </View>
   );
 };
